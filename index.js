6690c258e0b06fece1c3a2c7795dd784d17a2b32
UD <46685886+black-child-cat@users.noreply.github.com>
2023-05-03 02:03:13 +0900

    内容を修正

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ $(function () {
   $("form").submit(function () {
     var name = $('input[name="name"]').val();
     var customer = $('input[name="customer"]').val();
-    var detail = $('input[name="detail"]').val();
+    var detail = $('textarea[name="detail"]').val();
 
-    var msg = `源氏名：${name}\n相手の名前：${customer}\n詳細：${detail}`;
+    var msg = `お礼日記を考えてください。源氏名は「${name}」です。\n相手の名前は「${customer}」です。\n日記の内容は「${detail}」です。`;
     sendText(msg);
 
     return false;
