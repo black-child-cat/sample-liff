7225939b8dbc81c9b7ac46914833532af5cd5aea
UD <46685886+black-child-cat@users.noreply.github.com>
2023-05-03 01:41:43 +0900

    内容を調整

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 $(function () {
-  // カレンダー
-  $(function () {
-    $('input[name="date"]').datepicker({
-      dateFormat: "yy/mm/dd",
-    });
-  });
-
-  // 参加人数分の氏名欄を生成
-  $("#form-number").click(function () {
-    $("#form-name").empty();
-    var num = $('input[name="number"]:checked').val();
-    for (i = 0; i < num; i++) {
-      $("#form-name").append(
-        `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-      );
-    }
-  });
-
   // 送信
   $("form").submit(function () {
-    var date = $('input[name="date"]').val();
-    var number = $('input[name="number"]:checked').val();
-    var names = "";
-    $("#form-name")
-      .children()
-      .each(function (i, elm) {
-        names += $(elm).val() + "、";
-      });
-    names = names.slice(0, -1);
+    var name = $('input[name="name"]').val();
+    var customer = $('input[name="customer"]').val();
+    var detail = $('input[name="detail"]').val();
 
-    var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+    var msg = `源氏名：${name}\n相手の名前：${customer}\n詳細：${detail}`;
     sendText(msg);
 
     return false;
